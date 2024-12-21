dfe7b07d5db0197c0a44a5c4a131f3ac14393b4f
kashvigarg <kashvigarg02@gmail.com>
2024-12-21 20:49:36 +0530

    Refactor data/scripts

--- data/scripts/extract_mixed_corp.py
+++ data/scripts/extract_mixed_corp.py
@@ -1,3 +1,10 @@
+''' ------ Data Source ------
+HindEnCorp 0.5, Charles University
+
+[https://lindat.mff.cuni.cz/repository/xmlui/handle/11858/00-097C-0000-0023-625F-0]
+'''
+
+
 import csv
 
 def is_hindi(character):
@@ -6,28 +13,24 @@ def is_hindi(character):
         return True
     else:
       return False
-    
-with open("eng-hin-train.csv", "w", encoding='utf-8', newline='') as f:
-        writer = csv.writer(f)
-        with open("sentencepairs_csv.csv", "r", encoding='utf-8') as f:
-            csvFile = csv.reader(f)
-            for lines in csvFile:
-                lines.pop(0)
-                lines.pop(1)
-                writer.writerow(lines)
 
-with open("eng-hin-train2.csv", "w", encoding='utf-8', newline='') as f:
+with open("en-hi-train2.csv", "w", encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
-        
         with open("hindencorp05.plaintext", "r", encoding='utf-8') as fw:
             data = fw.read()
             data = data.split('\n')
+
+            # First 20k lines
             data = data[:20000]
             for lines in data:
                 lines = lines.split('\t')
+
+                # Remove metadata columns
                 lines.pop(0)
                 lines.pop(0)
                 lines.pop(0)
+
+                # Filter for non-hindi chars
                 hin = lines[1]
                 if not (is_hindi(hin)):
                      continue
--- data/scripts/extract_tsv.py
+++ data/scripts/extract_tsv.py
@@ -1,3 +1,9 @@
+''' ------ Data Source ------
+
+[https://tatoeba.org/en/downloads]
+'''
+
+
 import csv
 import pandas as pd
 
@@ -8,7 +14,8 @@ csv_form.to_csv('sentence_pairs_csv.csv', index=False)
 with open("sentence_pairs_csv.csv", "r", encoding='utf-8') as source: 
     reader = csv.reader(source) 
       
-    with open("output.csv", "w", encoding='utf-8', newline='') as result: 
+    with open("en-hi-train.csv", "w", encoding='utf-8', newline='') as result: 
         writer = csv.writer(result) 
         for r in reader: 
+            # Remove metadata columns
             writer.writerow((r[1], r[3]))
